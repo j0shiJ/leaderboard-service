68111b0ee0efc1bfb83465f238736817aa252eb6
Jayesh joshi <jayeshjoshi578@gmail.com>
2024-07-01 23:00:22 +0530

    Migrated whole application in docker container and used special port in docker-compose to interact with local db server

--- pkg/database/gorm.go
+++ pkg/database/gorm.go
@@ -2,14 +2,24 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
-	dsn := "host=localhost user=Jayesh password=jayesh dbname=leaderboard port=5432 sslmode=disable"
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+	// dsn := "host=localhost user=Jayesh password=jayesh dbname=leaderboard port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect database: %v", err)
